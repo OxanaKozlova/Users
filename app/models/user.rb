74a474006ca1cd585e5f757ed62d48e589eb7e9e
OxanaKozlova <aksana.kazlova@mtgarant.com>
2016-12-18 00:31:22 +0300

    add gem devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates :age, numericality: {greater_than: 0}
   validates :email, email_format: { :message => 'is not looking good' }
 end
