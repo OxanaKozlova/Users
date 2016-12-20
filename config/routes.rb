9b7fd554e893ac3b67b86b4de41993ca856a3881
OxanaKozlova <aksana.kazlova@mtgarant.com>
2016-12-20 22:08:53 +0300

    merge changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
   scope "/admin" do
     resources :users
-end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
